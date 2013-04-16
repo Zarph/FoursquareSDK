1e720eb403381a06ade80523a4247deca22621a9
Zarph <ramiro@leapcommerce.com>
2013-04-16 17:35:13 -0300

    Initial Commit

---
+++ FoursquareSDKExample/FoursquareAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FoursquareAppDelegate.h
+//  FoursquareSDKExample
+//
+//  Created by Ramiro Guerrero on 16/04/13.
+//  Copyright (c) 2013 Ramiro Guerrero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FoursquareAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FoursquareSDKExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FoursquareSDKExample
+//
+//  Created by Ramiro Guerrero on 16/04/13.
+//  Copyright (c) 2013 Ramiro Guerrero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FoursquareAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FoursquareAppDelegate class]));
+    }
+}
